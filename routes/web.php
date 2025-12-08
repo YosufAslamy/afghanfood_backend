21ff67dbd748f060331f9774f84aa16169f56f69
YosufAslamy <yosufaslamy9@gmail.com>
2025-12-08 15:56:39 +0430

    Income changes by omar to me

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,11 @@ Route::get('/', function () {
 });
 
 Route::post('/api/login', [LoginController::class, 'login']);
-//Menu Route by Omar
 Route::get('/api/menu', [MenuController::class, 'menu']);
+Route::post('/api/categories', [MenuController::class, 'addCategory']);
+Route::post('/api/foods', [MenuController::class, 'addfood']);
+Route::put('/api/categorie/{id}', [MenuController::class, 'updateCategory']);
+Route::put('/api/food/{id}', [MenuController::class, 'updateFood']);
+Route::delete('/api/categorie/{id}', [MenuController::class, 'deleteCategory']);
+Route::delete('/api/food/{id}', [MenuController::class, 'deleteFood']);
+Route::post('/api/message', [ContactController::class, 'submitMessage']);
