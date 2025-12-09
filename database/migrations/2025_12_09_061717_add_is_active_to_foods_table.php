486e23a488b903199fa785e860f0c20a2c70f6f9
Omar-rahat-007 <omarrahat100@gmail.com>
2025-12-09 15:13:47 +0430

    solved a bug

--- database/migrations/2025_12_09_061717_add_is_active_to_foods_table.php
+++ database/migrations/2025_12_09_061717_add_is_active_to_foods_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
 {
     Schema::table('foods', function (Blueprint $table) {
         $table->boolean('is_active')->default(true); 
+});
 }
 
 public function down()
